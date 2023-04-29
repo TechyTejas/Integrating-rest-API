704b3fb4bd7e1e78fdf327eaead0fdaf0e11554d
tejas <sadadetej.27@gmail.com>
2023-04-29 21:58:27 +0530

    each compartment added

--- proj.js
+++ proj.js
@@ -1,15 +1,14 @@
-//const listItem = document.getElementById("items")
 
 async function submitHandler(event) {
     try {
         event.preventDefault();
         const price = document.getElementById('price').value;
         const item = document.getElementById('item').value;
-        const category = document.getElementById('types').value;
+        const category = document.getElementById('typess').value;
 
         const obj = { price, item, category }
 
-        await axios.post("https://crudcrud.com/api/0fd30fc4985648fcb4a8758ce82e5a6b/Itemlist", obj);
+        await axios.post("https://crudcrud.com/api/543751b925074bf1863cf1c521a04cd2/Itemlist", obj);
         displayItemOnScreen();
         event.target.reset();
     }
@@ -19,12 +18,26 @@ async function submitHandler(event) {
 }
 
 async function displayItemOnScreen() {
-    const listItem=document.getElementById('items');
-    listItem.innerHTML="";
-    const response = await axios.get("https://crudcrud.com/api/0fd30fc4985648fcb4a8758ce82e5a6b/Itemlist")
-    let appoinments = response.data
-    for (let i = 0; i < appoinments.length; i++) {
-        const appointment = appoinments[i];
+    //selector tags
+    const a=document.getElementById("elect");
+    const b=document.getElementById("skinc");
+    const c=document.getElementById("foody");
+
+
+    //ul tags 
+    const Ele=document.getElementById('ele');
+    const Skin=document.getElementById('skin');
+    const Food=document.getElementById('food');
+    
+    //ul tags
+    Ele.innerHTML="";
+    Skin.innerHTML="";
+    Food.innerHTML="";
+
+    const response = await axios.get("https://crudcrud.com/api/543751b925074bf1863cf1c521a04cd2/Itemlist")
+    let lists = response.data
+    for (let i = 0; i < lists.length; i++) {
+        const order = lists[i];
 
         const newItem = document.createElement('li');
 
@@ -33,16 +46,33 @@ async function displayItemOnScreen() {
         deletebtn.type = 'button';
         deletebtn.value = 'Delete';
         deletebtn.onclick = async () => {
-            await axios.delete(`https://crudcrud.com/api/0fd30fc4985648fcb4a8758ce82e5a6b/Itemlist/${appointment._id}`)
-            listItem.removeChild(newItem);
+            await axios.delete(`https://crudcrud.com/api/543751b925074bf1863cf1c521a04cd2/Itemlist/${appointment._id}`)
+
+           
+            if(order.category==a.value){
+                Ele.removeChild(newItem)
+            }
+            else if(order.typess==b.value){
+                Skin.removeChild(newItem)
+            }
+            else{
+                Food.removeChild(newItem);
+            }
         }
 
-
-        newItem.textContent = appointment.price + '-' + appointment.item + '-' + appointment.category;
+    if(order.category==a.value){
+        Ele.appendChild(newItem)
+    }
+    else if(order.category==b.value){
+        Skin.appendChild(newItem)
+    }
+    else{
+        Food.appendChild(newItem);
+    }
+    
+    newItem.textContent = order.price + '-' + order.item + '-' + order.category;
         
-        console.log(appointment)
-        newItem.appendChild(deletebtn);
-        listItem.appendChild(newItem);
+    newItem.appendChild(deletebtn);
     }
 }
 
